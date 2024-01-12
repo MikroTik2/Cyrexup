0b666edce7e3c3569bc62154a1a859db8822910f
vladislav2251 <vladdidyk1985@gmail.com>
2024-01-12 22:09:11 +0200

    responsive_index

--- client/src/components/IndexItem.vue
+++ client/src/components/IndexItem.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="max-w-full w-full mx-auto p-[5px_15px] md:p-[10px__25px] bg-neutral-800 bg-opacity-40 rounded-[10px] backdrop-blur-[20px]">
         <!-- avatar -->
-        <div class="flex gap-2.5 items-center mx-auto">
+        <div class="flex md:max-lg:justify-between gap-2.5 items-center mx-auto">
             <img class="w-[25px] h-[25px] md:w-[45px] md:h-[45px]" src="/img/rounded-avatar.png" alt="avatar">
             <!-- there desc -->
             <div class="grid">
-                <p class="text-[12px] md:text-[18px] font-bold">Studio</p>
+                <p class="text-[12px] md:text-[18px] md:max-lg:text-right  font-bold">Studio</p>
                 <span class="text-neutral-500 text-[9px] md:text-sm font-semibold">Топ 2 сайта</span>
             </div>
         </div>
         <!-- title -->
-        <p class="text-[10px] md:text-[12px] text-center font-semibold mt-1 md:mt-2.5">Самый дорогой предмет</p>
+        <p class="text-[10px] md:text-[20px] text-center font-semibold mt-1 md:mt-2.5">Самый дорогой предмет</p>
         <!-- item and price -->
         <div class="relative w-full mx-auto">
-            <img class="mx-auto lg:pl-0 w-[75px] h-[75px] md:w-[137px] md:h-[137px]" src="/img/MAC-10.png" alt="mac">
+            <img class="mx-auto lg:pl-0 w-[75px] h-[75px] md:w-[150px] md:h-[150px]" src="/img/MAC-10.png" alt="mac">
             <div class="bg-neutral-600 bg-opacity-30 rounded-[10px] flex justify-center items-center w-[40px] md:w-[65px] h-[15px] md:h-[20px] text-[7px] md:text-[10px] font-bold absolute top-[76%] left-[55%] md:left-[50%] origin-[50%_50%]">312.231$</div>
         </div>
         <!-- result-title -->
-        <p class="text-[10px] md:text-[12px] text-center font-semibold md:mt-2.5">Выиграно за всё время</p>
+        <p class="text-[10px] md:text-[20px] text-center font-semibold md:mt-2.5">Выиграно за всё время</p>
         <!-- result -->
         <div class="mx-auto bg-neutral-600 bg-opacity-30 rounded-[10px] flex justify-center items-center w-[80px] h-[20px] md:h-[30px] md:w-[100px] text-[10px] md:text-[16px] font-bold mt-[4px] md:mt-[5px]">312.231$</div>
     </div>
--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -1,40 +1,40 @@
 <template>
      <div class="w-full mx-auto">
-          <SliderIndex class="2xl:w-[1440px] min-w-[220px]"/>
-          <div class="max-w-full mx-auto w-[1440px] grid xl:grid-cols-2 gap-2.5 md:gap-[15px] mt-2">
-               <div class="w-full bg-[#16191B] p-2.5 rounded-[10px] mx-auto">
-                    <div class="grid w-[200px] sm:w-[282px] md:w-[415px]">
+          <SliderIndex class="2xl:w-[1440px]  sm:min-w-[240px]"/>
+          <div class="max-w-full mx-auto w-[1440px] grid xl:grid-cols-2 gap-2.5 md:gap-[.9375rem] mt-2">
+               <div class="w-full bg-[#16191B] p-2.5 rounded-[.625rem] mx-auto">
+                    <div class="grid w-[12.5rem] sm:w-[17.625rem] md:w-[25.9375rem]">
                          <!-- title -->
-                         <h2 class="text-[13px] sm:text-[15px] md:text-[30px] font-bold">Активные конкурсы</h2>
+                         <h2 class="text-[.8125rem] sm:text-[.9375rem] md:text-[1.875rem] font-bold">Активные конкурсы</h2>
                          <!-- desc -->
-                         <p class="text-[6px] sm:text-[8px]  md:text-[14px] text-[var(--gray-800)] mt-[3px] sm:mt-[5px] lg:mr-2.5">Сейчас имеется 3 интереснейших конкурса с полезными призами, которые точно вас заинтересуют. Мы предлагаем один ежемесячный, еженедельный и ежедневный конкурс, а точнее ваш уникальный шанс получить награду. Для этого вам всего лишь нужно выполнить легчайшие условия, которые точно всем под силу. Дальше дело за малым. Просто нужно набраться немного терпения, уверенности и удачи для получения желаемого приза.</p>
+                         <p class="text-[.375rem] sm:text-[.5rem]  md:text-[.875rem] text-[var(--gray-800)] mt-[.1875rem] sm:mt-[.3125rem] lg:mr-2.5">Сейчас имеется 3 интереснейших конкурса с полезными призами, которые точно вас заинтересуют. Мы предлагаем один ежемесячный, еженедельный и ежедневный конкурс, а точнее ваш уникальный шанс получить награду. Для этого вам всего лишь нужно выполнить легчайшие условия, которые точно всем под силу. Дальше дело за малым. Просто нужно набраться немного терпения, уверенности и удачи для получения желаемого приза.</p>
                          <!-- timer-title -->
-                         <h2 class="text-[9px] sm:text-xs md:text-[17px] font-bold mt-1 sm:mt-2.5 md:mt-[25px]">Время до обновления ежемечного конкурса:</h2>
+                         <h2 class="text-[.5625rem] sm:text-xs md:text-[1.0625rem] font-bold mt-1 sm:mt-2.5 md:mt-[1.5625rem]">Время до обновления ежемечного конкурса:</h2>
                          <!-- timer -->
                          <div class="flex items-center gap-2.5 mt-1 md:mt-2.5">
                               <img class="w-3.5  md:w-6 md:h-6" src="/svg/clock.svg" alt="timer">
-                              <p class="text-[10px] sm:text-xs md:text-[18px] font-bold ">03д  <span class="text-[var(--gray-900)] font-bold">:</span>  02ч  <span class="text-[var(--gray-900)]">:</span>  33м  <span class="text-[var(--gray-900)]">:</span>  45с</p>
+                              <p class="text-[.625rem] sm:text-xs md:text-[1.125rem] font-bold ">03д  <span class="text-[var(--gray-900)] font-bold">:</span>  02ч  <span class="text-[var(--gray-900)]">:</span>  33м  <span class="text-[var(--gray-900)]">:</span>  45с</p>
                          </div>
                          <!-- line-timer -->
-                         <img class="w-[200px] sm:w-[224px] md:w-[279px] h-[5px] mt-3 md:mt-[20px]" src="/svg/line-timer.svg" alt="timer">
+                         <img class="w-[12.5rem] sm:w-[14rem] md:w-[17.4375rem] h-[.3125rem] mt-3 md:mt-[1.25rem]" src="/svg/line-timer.svg" alt="timer">
                          <!-- button -->
-                         <button class="w-[100px] sm:w-[130px] md:w-[165px] h-[25px] sm:h-[30px] md:h-[40px] text-[var(--blue-400)] bg-[var(--black-400)] justify-center flex items-center gap-[3px] rounded-[5px] font-bold text-[10px] sm:text-xs md:text-[15px] mt-2.5 md:mt-[35px]">
+                         <button class="w-[6.25rem] sm:w-[8.125rem] md:w-[10.3125rem] h-[1.5625rem] sm:h-[1.875rem] md:h-[2.5rem] text-[var(--blue-400)] bg-[var(--black-400)] justify-center flex items-center gap-[.1875rem] rounded-[.3125rem] font-bold text-[.625rem] sm:text-xs md:text-[.9375rem] mt-2.5 md:mt-[2.1875rem]">
                               <p>Участвовать</p>
-                              <img class="w-[13px] sm:w-[18px]" src="/svg/arrow.svg" alt="arrow">
+                              <img class="w-[.8125rem] sm:w-[1.125rem]" src="/svg/arrow.svg" alt="arrow">
                          </button>
                          <!-- <div class="max-w-full w-">
                               <SliderItems/>
                          </div> -->
                     </div>
                </div>
-               <div class="w-full w xl:w-[760px] bg-[#16191B] rounded-[10px] mx-auto mt-1 md:mt-0 p-[10px] md:p-[15px] lg:p-[20px]">
+               <div class="w-full bg-[#16191B] rounded-[.625rem] mx-auto mt-1 md:mt-0 p-[.625rem] md:p-[.9375rem] lg:p-[1.25rem]">
                <!-- title -->
                <div class="w-full mx-auto">
-                    <h2 class="text-[10px] sm:text-[15px] md:text-[25px] lg:text-[30px] text-3xl font-extrabold uppercase text-center mb-[10px]">Топ игроков</h2>
+                    <h2 class="text-[.625rem] sm:text-[.9375rem] md:text-[1.5625rem] lg:text-[1.875rem] text-3xl font-extrabold uppercase text-center md:mb-[.625rem]">Топ игроков</h2>
                     <!-- absolute block top-[60%] left-[35%] xl:left-[35%] oriin-[50%_50%] -->
                </div>
                <!-- item -->
-               <div class="inline-flex gap-5 mx-auto w-full">
+               <div class="grid grid-cols-2 sm:grid-cols-3 gap-2 md:gap-5 mx-auto w-full">
                     <IndexItem/>
                     <IndexItem/>
                     <IndexItem/>
