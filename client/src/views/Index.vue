97f5fbe80ab468d51870f87ecc87169d5d90d491
vladislav2251 <vladdidyk1985@gmail.com>
2024-01-12 23:13:57 +0200

    responsive_index_fix

--- client/src/views/Index.vue
+++ client/src/views/Index.vue
@@ -1,55 +1,6 @@
 <template>
-     <div class="w-full mx-auto">
-          <SliderIndex class="2xl:w-[1440px]  sm:min-w-[240px]"/>
-          <div class="max-w-full mx-auto w-[1440px] grid xl:grid-cols-2 gap-2.5 md:gap-[.9375rem] mt-2">
-               <div class="w-full bg-[#16191B] p-2.5 rounded-[.625rem] mx-auto">
-                    <div class="grid w-[12.5rem] sm:w-[17.625rem] md:w-[25.9375rem]">
-                         <!-- title -->
-                         <h2 class="text-[.8125rem] sm:text-[.9375rem] md:text-[1.875rem] font-bold">Активные конкурсы</h2>
-                         <!-- desc -->
-                         <p class="text-[.375rem] sm:text-[.5rem]  md:text-[.875rem] text-[var(--gray-800)] mt-[.1875rem] sm:mt-[.3125rem] lg:mr-2.5">Сейчас имеется 3 интереснейших конкурса с полезными призами, которые точно вас заинтересуют. Мы предлагаем один ежемесячный, еженедельный и ежедневный конкурс, а точнее ваш уникальный шанс получить награду. Для этого вам всего лишь нужно выполнить легчайшие условия, которые точно всем под силу. Дальше дело за малым. Просто нужно набраться немного терпения, уверенности и удачи для получения желаемого приза.</p>
-                         <!-- timer-title -->
-                         <h2 class="text-[.5625rem] sm:text-xs md:text-[1.0625rem] font-bold mt-1 sm:mt-2.5 md:mt-[1.5625rem]">Время до обновления ежемечного конкурса:</h2>
-                         <!-- timer -->
-                         <div class="flex items-center gap-2.5 mt-1 md:mt-2.5">
-                              <img class="w-3.5  md:w-6 md:h-6" src="/svg/clock.svg" alt="timer">
-                              <p class="text-[.625rem] sm:text-xs md:text-[1.125rem] font-bold ">03д  <span class="text-[var(--gray-900)] font-bold">:</span>  02ч  <span class="text-[var(--gray-900)]">:</span>  33м  <span class="text-[var(--gray-900)]">:</span>  45с</p>
-                         </div>
-                         <!-- line-timer -->
-                         <img class="w-[12.5rem] sm:w-[14rem] md:w-[17.4375rem] h-[.3125rem] mt-3 md:mt-[1.25rem]" src="/svg/line-timer.svg" alt="timer">
-                         <!-- button -->
-                         <button class="w-[6.25rem] sm:w-[8.125rem] md:w-[10.3125rem] h-[1.5625rem] sm:h-[1.875rem] md:h-[2.5rem] text-[var(--blue-400)] bg-[var(--black-400)] justify-center flex items-center gap-[.1875rem] rounded-[.3125rem] font-bold text-[.625rem] sm:text-xs md:text-[.9375rem] mt-2.5 md:mt-[2.1875rem]">
-                              <p>Участвовать</p>
-                              <img class="w-[.8125rem] sm:w-[1.125rem]" src="/svg/arrow.svg" alt="arrow">
-                         </button>
-                         <!-- <div class="max-w-full w-">
-                              <SliderItems/>
-                         </div> -->
-                    </div>
-               </div>
-               <div class="w-full bg-[#16191B] rounded-[.625rem] mx-auto mt-1 md:mt-0 p-[.625rem] md:p-[.9375rem] lg:p-[1.25rem]">
-               <!-- title -->
-               <div class="w-full mx-auto">
-                    <h2 class="text-[.625rem] sm:text-[.9375rem] md:text-[1.5625rem] lg:text-[1.875rem] text-3xl font-extrabold uppercase text-center md:mb-[.625rem]">Топ игроков</h2>
-                    <!-- absolute block top-[60%] left-[35%] xl:left-[35%] oriin-[50%_50%] -->
-               </div>
-               <!-- item -->
-               <div class="grid grid-cols-2 sm:grid-cols-3 gap-2 md:gap-5 mx-auto w-full">
-                    <IndexItem/>
-                    <IndexItem/>
-                    <IndexItem/>
-               </div>
-           </div>
-          </div>
-     </div>
+     <IndexContent/>
 </template>
 <script setup>
-     import SliderIndex from '@/components/SliderIndex.vue';
-     import SliderItems from '@/components/SliderItems.vue';
-     import IndexItem from '@/components/IndexItem.vue';
-     import Button from '@/components/button/Button.vue';
-
-const props = defineProps({
-     colorLink: String,
-});
+     import IndexContent from '@/components/IndexContent.vue';
 </script>
